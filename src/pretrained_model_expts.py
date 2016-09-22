9b76534a1b8ab6ce8f243d1fbc3052cf34ded301
Viswanadh Rayavarapu <rnviswanadh291995@gmail.com>
2016-09-22 22:39:50 +0530

    Features file

--- src/pretrained_model_expts.py
+++ src/pretrained_model_expts.py
@@ -1,6 +1,8 @@
 # from keras import backend as K
 from data_provision_att_vqa import *
 if __name__ == '__main__':
-	data_provision_att_vqa = DataProvisionAttVqa("../data_vqa", "")
+	print "Started Loading data"
+	data_provision_att_vqa = DataProvisionAttVqa("../data_vqa", "trainval_feat.h5")
+	print "Loaded Data"
 	# input = K.placeholder(shape=(None, 196, 512))
 	print data_provision_att_vqa
